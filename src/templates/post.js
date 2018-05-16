a5c57534d1bc6f4ee3c2339f7cc506e133b63402
Pierre G. 🚧 <imacmunso@iMac-de-Munso.local>
2018-05-16 11:10:06 +0200

    new req graphQl

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const Post = () => {
+const Post = ({ data }) => {
+  const { title, content, date } = data.wordpressPost
   return (
-    <h1>Un post</h1>
+    <div>
+      <h1>{title}</h1>
+      <p>{date}</p>
+      <div dangerouslySetInnerHTML={{ __html: content}} />
+    </div>
   )
 }
 
 export default Post
+
+export const postQuery = graphql`
+  query postQuery($id: String!){
+    wordpressPost(id: {eq: $id}) {
+      title
+      content
+      date(formatString: "DD/MM/YYYY")
+    }
+  }
+`
